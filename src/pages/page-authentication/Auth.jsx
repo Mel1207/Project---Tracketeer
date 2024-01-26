580abd7a9ce0f206bf21c087a535463d1487ae56
Mel <57514727+Mel1207@users.noreply.github.com>
2024-01-26 17:19:33 +0800

    fix login page ui

--- src/pages/page-authentication/Auth.jsx
+++ src/pages/page-authentication/Auth.jsx
@@ -3,6 +3,8 @@ import iconGoogle from '../../assets/icon-google.svg'
 import { auth, provider } from '../../config/firebase-config' 
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import bgLogin from '../../assets/img-login-bg.jpg'
+import loginLogo from '../../assets/icon-login.png'
 
 const Auth = () => {
   const navigate = useNavigate()
@@ -23,12 +25,13 @@ const Auth = () => {
 
   return (
     <div className='login-page'>
-      <div>
-
-      </div>
-      <div>
-        <h1>Welcome back Tracketeer 👋</h1>
-        <button onClick={handleSignInGoogle}><img src={iconGoogle} alt="google icon" />Sign in with Google</button>
+      <img src={bgLogin} alt="Login Background" className='login-left'/>
+      <div className='login-right'>
+        <div className='login-container'>
+          <img src={loginLogo} alt="Logo" className='login-logo'/>
+          <h1>Welcome back Tracketeer 👋</h1>
+          <button onClick={handleSignInGoogle} className='btn google-login'><img src={iconGoogle} alt="google icon" />Sign in with Google</button>
+        </div>
       </div>
       
     </div>
