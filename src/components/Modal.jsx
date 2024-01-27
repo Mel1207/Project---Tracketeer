99583843779d4cdf5baf8ae17ea6a542c9cfd3e7
Mel <57514727+Mel1207@users.noreply.github.com>
2024-01-27 23:55:14 +0800

    added reusable modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,30 +1,16 @@
 import React from 'react'
 import iconClose from '../assets/icon-close.svg'
 
-const Modal = () => {
+const Modal = ({ children, modalTitle }) => {
   return (
     <div className='modal'>
       <div className="modal-container">
         <div className="modal-header">
-          <h3>New Transaction</h3>
+          <h3>{modalTitle}</h3>
           <img src={iconClose} alt="Close" className='modal-close'/>
         </div>
         <div className="modal-content">
-          <div className="form-group">
-            <p>Transaction</p>
-            <input type="text" placeholder='Enter Transaction' />
-          </div>
-          <div className="form-group">
-            <p>Type</p>
-            <select id="transaction-type">
-              <option value="income">Income</option>
-              <option value="expense">Expense</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <p>Amount</p>
-            <input type="number" placeholder='0.00' />
-          </div>
+          {children}
         </div>
         <div className="modal-footer">
           <button className='btn btn-secondary'>Cancel</button>
