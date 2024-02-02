2f95959902738f71d655f674ab3a35b53b03a863
Mel <57514727+Mel1207@users.noreply.github.com>
2024-02-02 11:34:39 +0800

    added profile page feature

--- src/pages/page-profile/ProfilePage.jsx
+++ src/pages/page-profile/ProfilePage.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Sidebar from '../../components/Sidebar'
+import { useGetUserInfo } from '../../hooks/useGetUserInfo'
 
 const ProfilePage = () => {
+  const { name, userId, avatar } = useGetUserInfo()
+
   return (
     <>
       <Navbar />
       <Sidebar />
       <main>
         <div className='container'>
-          <h3>Mel John Pualon</h3>
-          <p>Profile Information</p>
+          <div className="profile-card">
+            <img src={avatar} alt={name} className='avatar'/>
+            <h3 className='profile-name'>{name}</h3>
+            <p className='profile-role'>Admin</p>
+            <div className='profile-details'>
+              <span>Email: <p>feature soon...</p></span>
+              <span>Enable dark mode: <p>feature soon...</p></span>
+              <span>Work: <p>feature soon...</p></span>
+            </div>
+          </div>
         </div>
       </main>
     </>
